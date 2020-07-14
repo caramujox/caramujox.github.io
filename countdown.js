c394d50722c2317c2036d3237a77384bae8d1375
Caio de Araujo <caioa.morais@gmail.com>
2020-07-14 11:21:46 -0300

    Teste DOM rendering order

--- countdown.js
+++ countdown.js
@@ -9,10 +9,12 @@ var myfunc = setInterval(function () {
     var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
+    document.getElementById("msg1").innerHTML = "Aguarde, faltam "
     document.getElementById("days").innerHTML = days + " dias <br/>"
     document.getElementById("hours").innerHTML = hours + " horas <br/>"
     document.getElementById("mins").innerHTML = minutes + " minutos <br/>"
-    document.getElementById("secs").innerHTML = seconds + " segundos"
+    document.getElementById("secs").innerHTML = seconds + " segundos </br>"
+    document.getElementById("msg3").innerHTML = " para você se emocionar!"
 
     if (timeleft < 0) {
         window.location.replace("https://www.youtube.com/watch?v=om3n2ni8luE&list=RDom3n2ni8luE&start_radio=1")
